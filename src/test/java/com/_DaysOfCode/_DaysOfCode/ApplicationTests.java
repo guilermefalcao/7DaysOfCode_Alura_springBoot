7b2d653e9ef1e9432a76e6e60100db5e668327d3
Guilherme Falcao <guilherme.falcao@dataprev.gov.br>
2026-01-17 17:48:55 -0300

    Projeto inicial Spring Boot - 7 Days of Code

---
+++ src/test/java/com/_DaysOfCode/_DaysOfCode/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com._DaysOfCode._DaysOfCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
